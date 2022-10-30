fdf235450c91a6f19d1425dfcec3ec543af9c803
Javith77 <javimemo97@gmail.com>
2022-10-30 15:26:29 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const express = require('express');
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + '/dist/FRONT_SCHOOL_CALENDAR'));
+app.use(express.static('./dist/FRONT_SCHOOL_CALENDAR'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/FRONT_SCHOOL_CALENDAR/index.html'));
+  res.sendFile('index.html', {root: 'dist/FRONT_SCHOOL_CALENDAR/'});
 });
 
-// default Heroku port
+// start app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
