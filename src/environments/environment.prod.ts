80e5bc4a51555a2df1a1f8ad0aef32186436c92f
Javith77 <javimemo97@gmail.com>
2022-10-30 15:15:42 -0500

    Config to PDN

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  API_TEACHER: 'https://backend-schedule-calendar.herokuapp.com/api/teachers',
+  API_STUDENT: 'https://backend-schedule-calendar.herokuapp.com/api/students',
+  API_COURSE: 'https://backend-schedule-calendar.herokuapp.com/api/courses',
+  API_ACADEMIC_SUBJECT: 'https://backend-schedule-calendar.herokuapp.com/api/academic-subjects',
+  API_SCHEDULE: 'https://backend-schedule-calendar.herokuapp.com/api/schedule',
 };
