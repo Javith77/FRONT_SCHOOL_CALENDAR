6ac78fc975366f638c4bf3b370d9c9aaf1b3ab05
Javith77 <javimemo97@gmail.com>
2022-10-30 16:34:48 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,13 @@
 const express = require('express');
-const app = express();
 const path = require('path');
-const port = process.env.PORT || 8000;
-const server = require('http').Server(app);
-
-app.use(express.static(__dirname, 'dist', {index: false}));
-
-//start app by listening on the default Heroku port
-server.listen(port, function() {
-  console.log("App running on port " + port);
-})
+const app = express();
 
 // Serve static files
-app.use(express.static(path.join(__dirname, 'dist','FRONT_SCHOOL_CALENDAR')));
-
+app.use(express.static(__dirname + '/dist'));
 // Send all requests to index.html
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'dist', 'FRONT_SCHOOL_CALENDAR', 'index.html'));
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
+
+//start app by listening on the default Heroku port
+app.listen(process.env.PORT || 4200);
